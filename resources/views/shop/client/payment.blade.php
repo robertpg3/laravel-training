e06ec2735797a8a1ac9a4e48a6250810a5f26a7b
robertpg3 <robertpgherghel@gmail.com>
2021-09-21 13:51:01 +0300

    payment design

--- resources/views/shop/client/payment.blade.php
+++ resources/views/shop/client/payment.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.client_layout')
 
 @section('content')
-    <div>
-        <input id="cardholder-name" type="text" data-secret="{{ $user_email }}">
-        <div id="card-element"></div>
-        <button id="card-button" data-secret="{{ $intent->client_secret }}">
-            Submit Payment
-        </button>
-    </div>
+        <div class="payment-container">
+            <div class="payment-data">
+                <input id="cardholder-name" class="card-input" type="text" placeholder="Name" data-secret="{{ $user_email }}">
+                <div id="card-element" class="card-input"></div>
+            </div>
+            <button id="card-button" class="card-input" data-secret="{{ $intent->client_secret }}">
+                Submit Payment
+            </button>
+        </div>
 @endsection
